023d84dcd06bf03fe3d7426ceb8401e95848e870
jaime-ribeiro <littlejou@gmail.com>
2023-01-12 19:49:52 -0300

    Challenge Complete :white_check_mark:

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,34 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {
+      relations: ["games"]
+    })
+    //left join
+
+    if (!user){
+      throw new Error("User does not exist!")
+    }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+    SELECT *
+    FROM users
+    ORDER BY first_name ASC
+    `); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`
+    SELECT *
+    FROM users
+    WHERE UPPER(first_name) = UPPER('${first_name}') AND UPPER(last_name) = UPPER('${last_name}')
+    `); // Complete usando raw query
   }
 }
